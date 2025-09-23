2dc23c399d7c940c208c5d3fd3f7ee8208d5779a
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-09-23 11:35:28 +0300

    Pabeigta klase Cetrsturis

--- src/uzdevums5/Cetrsturis.java
+++ src/uzdevums5/Cetrsturis.java
@@ -1,5 +1,27 @@
 package uzdevums5;
 
 public class Cetrsturis {
-
+	public double platums, augstums;
+	public Centrs punkts;
+	
+	// Konstruktoru pārslogošana
+	public Cetrsturis() {
+		punkts = new Centrs (0, 0);
+	}
+	
+	public Cetrsturis(Centrs punkts, double p, double a) {
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+	}
+	
+	public String izvaditCetrsturaInfo() {
+		return "Platums: "+platums+"cm augstums: "+augstums+"cm";
+	}
+	
+	// Uztaisīt metodi, kas aprēķina laukumu
+	public double cetrsturaLaukums() {
+		double S = platums * augstums;
+		return S;
+	}
 }
