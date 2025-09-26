6461772255bd4696199cbbc28086ab1f29097af0
LiteDuck1986 <emils2007smirnovs@gmail.com>
2025-09-26 09:21:29 +0300

    Iesākta OIzveide klase

--- src/uzdevums5/Figuras.java
+++ src/uzdevums5/Figuras.java
@@ -1,27 +1,68 @@
 package uzdevums5;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import uzd1.MinkuTante;
 
 public class Figuras {
+	static ArrayList<Centrs> centraObjekti = new ArrayList<>();
+	static ArrayList<Cetrsturis> cetrsturaObjekti = new ArrayList<>();
+	static ArrayList<Aplis> aplaObjekti = new ArrayList<>();
+	static ArrayList<Trijsturis> trijsturaObjekti = new ArrayList<>();
+	
+	static String[] ObjektuPogas = {"Centrs", "Četrstūris", "Aplis", "Trijsturis", "Atgriezties"};
 
 	public static void main(String[] args) {
 		// Izveido Centrs objektu
-		Centrs centraP1 = new Centrs(-10, 12);
-		System.out.println(centraP1.izvaditKoordinatas());
-		centraP1.parvietot(5, -2);
-		System.out.println(centraP1.izvaditKoordinatas());
+//		Centrs centraP1 = new Centrs(-10, 12);
+//		System.out.println(centraP1.izvaditKoordinatas());
+//		centraP1.parvietot(5, -2);
+//		System.out.println(centraP1.izvaditKoordinatas());
+//		
+//		// Izveido Cetrsturis objektu
+//		Cetrsturis cetrsturis1 = new Cetrsturis();
+//		System.out.println(cetrsturis1.izvaditCetrsturaInfo());
+//		
+//		// Izvadīt ši četrstūra atrašanās vietu
+//		System.out.println(cetrsturis1.punkts.izvaditKoordinatas());
+//		
+//		Cetrsturis cetrsturis2 = new Cetrsturis(centraP1, 5, 11);
+//		System.out.println(cetrsturis2.izvaditCetrsturaInfo()+"\n"+cetrsturis2.punkts.izvaditKoordinatas()+"\ns = "+cetrsturis2.cetrsturaLaukums());
+//
+//		Centrs punkts2 = new Centrs (5, 23);
+//		Aplis aplis1 = new Aplis(punkts2, 5);
+//		System.out.println(aplis1.izvaditAplaInfo()+"\n"+aplis1.rLaukums()+"\n"+"C = "+ aplis1.RinkaLinija());
+//	}
+	
+	
+	int izvele, poga;
+	
+	
+	
+	do {
+		izvele = MinkuTante.skaitlaParbaude(
+				"Ko vēlies darīt?\n"
+				+ "0 - Apturēt\n"
+				+ "1 - Izveidot objektu\n"
+				+ "2 - Aplūkot objektus\n"
+				+ "3 - Izsaukt metodi", 0, 3);
+		if (izvele == -1)
+			izvele = 0;
 		
-		// Izveido Cetrsturis objektu
-		Cetrsturis cetrsturis1 = new Cetrsturis();
-		System.out.println(cetrsturis1.izvaditCetrsturaInfo());
+		switch(izvele) {
 		
-		// Izvadīt ši četrstūra atrašanās vietu
-		System.out.println(cetrsturis1.punkts.izvaditKoordinatas());
-		
-		Cetrsturis cetrsturis2 = new Cetrsturis(centraP1, 5, 11);
-		System.out.println(cetrsturis2.izvaditCetrsturaInfo()+"\n"+cetrsturis2.punkts.izvaditKoordinatas()+"\ns = "+cetrsturis2.cetrsturaLaukums());
-
-		Centrs punkts2 = new Centrs (5, 23);
-		Aplis aplis1 = new Aplis(punkts2, 5);
-		System.out.println(aplis1.izvaditAplaInfo()+"\n"+aplis1.rLaukums()+"\n"+"C = "+ aplis1.rLaukums());
-	}
+		case 0:
+			JOptionPane.showMessageDialog(null, "Programma apturēta!", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			break;
+			
+		case 1:
+			OIzveide.izveidotObjektu();
+			break;
+			
+		}
+	} while (izvele != 0);
+	
+}
 
 }
